581e0aded6daae7dcf86a44422a9fc6931154735
Carlos Ríos Román <21cjrr@gmail.com>
2024-04-18 21:28:39 -0600

    Menu
    Fixing function of the Menu in 500px

--- js/home.js
+++ js/home.js
@@ -1,12 +1,26 @@
 const toggleBtn = document.getElementById('toggle_btn');
 const menu = document.getElementById('menu');
-const menuIcon = document.getElementById('toggle_btn');
+const icon = document.getElementById('icon');
 
 
 //Abrir el menu
 toggleBtn.addEventListener('click', function() {
-  menu.classList.toggle('open');
+
+  if(menu.style.display != 'block'){
+    menu.style.display = 'block';
+    icon.classList.add('fa-x');
+    icon.classList.remove('fa-bars');
+  } else {
+    menu.style.display = 'none';
+    icon.classList.remove('fa-x');
+    icon.classList.add('fa-bars');
+  }
+
+   
+ 
+  
 });
 
+//Cambiar icono de submenu
 
 
