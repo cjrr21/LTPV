352136023e67fce11b0394b6bda4573d17e34796
Carlos Ríos Román <21cjrr@gmail.com>
2024-04-22 23:32:38 -0600

    Slider
    Slider working in Adventure Page

--- js/home.js
+++ js/home.js
@@ -1,7 +1,9 @@
 const toggleBtn = document.getElementById('toggle_btn');
 const menu = document.getElementById('menu');
 const icon = document.getElementById('icon');
-
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 1;
+const place = document.getElementById('caption');
 
 //Abrir el menu
 toggleBtn.addEventListener('click', function() {
@@ -17,5 +19,27 @@ toggleBtn.addEventListener('click', function() {
   }
 });
 
+//Slider
+function showSlides(n) {
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
 
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+    slides[slideIndex - 1].style.display = 'block';
+    place.textContent = '';
+    place.style.transform = 'translateY(100%) translateX(0%)';
+    
+    setTimeout(() => {
+    place.textContent = slides[slideIndex-1].getAttribute("alt");
+    place.style.transform = 'translateY(0) translateX(0%)';
+    }, 1500);
 
+}
+
+function plusSlides(n) {showSlides(slideIndex += n);}
+
+showSlides(slideIndex);
+document.querySelector('.prev').addEventListener('click', () => {plusSlides(-1);});
+document.querySelector('.next').addEventListener('click', () => {plusSlides(1);});
